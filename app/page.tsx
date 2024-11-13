ea76acfe7d1bf6bb5ffadb8c5584cbbda7e4937c
Paul Le <paul@investorhub.com>
2024-11-14 03:48:11 +1100

    Add some links and update Typewriter to take an array

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,31 @@
 "use client";
 
-// import Image from "next/image";
-import Typewriter from "./typewriter";
+import Typewriter from "@components/typewriter";
+import Header from "@components/header";
 
 const Home: React.FC = () => {
+  const date = new Date();
+
+  const texts = [
+    new Intl.DateTimeFormat("en-US", { dateStyle: "long" }).format(date),
+    new Intl.DateTimeFormat("es", { dateStyle: "long" }).format(date),
+    new Intl.DateTimeFormat("ko", { dateStyle: "long" }).format(date),
+    new Intl.DateTimeFormat("fr", { dateStyle: "long" }).format(date),
+    new Intl.DateTimeFormat("vi", { dateStyle: "long" }).format(date),
+    new Intl.DateTimeFormat("zh", { dateStyle: "long" }).format(date),
+    new Intl.DateTimeFormat("ja", { dateStyle: "long" }).format(date),
+    new Intl.DateTimeFormat("de", { dateStyle: "long" }).format(date),
+    new Intl.DateTimeFormat("it", { dateStyle: "long" }).format(date),
+    new Intl.DateTimeFormat("id", { dateStyle: "long" }).format(date),
+  ];
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-mono)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center justify-center sm:items-start w-full">
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-mono)]">
+      <main className="flex flex-col items-center justify-center gap-16 w-full">
         <div className="p-8 w-full flex items-center justify-center">
-          {/* TODO: Something else */}
-          <Typewriter text="Welcome to the digital garden" infinite />
+          <Typewriter texts={texts} infinite />
         </div>
-        {/* <div className="p-8 w-full flex items-center justify-center">
-          <Image
-            width={400}
-            height={300}
-            src={"/flowers.webp"}
-            alt="Field of Flowers"
-            className="w-full h-auto"
-          />
-        </div> */}
+        <Header />
       </main>
     </div>
   );
